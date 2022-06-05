0782493626fc9d72466638426819dd7a2091a02a
zangxin51 <98462129+zangxin51@users.noreply.github.com>
2022-06-05 17:12:08 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test ");
         System.out.println("hello test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
